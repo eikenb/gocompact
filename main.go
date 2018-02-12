425cf0204459f22b9a48e2a2906c7a36ec67f2f8
John Eikenberry <jae@zhar.net>
2018-02-12 00:11:24 -0800

    initial commit of simple main
    Fixes imports in the AST then prints the source.
    Simple gofmt type functionality w/ no options.

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"io/ioutil"
+	"os"
+
+	"gocompact/printer"
+
+	// "golang.org/x/tools/imports"
+	"github.com/eikenb/tools/imports"
+)
+
+const printerMode = printer.UseSpaces | printer.TabIndent
+
+var cfg = printer.Config{Mode: printerMode, Tabwidth: 4}
+
+func main() {
+	flag.Parse()
+	paths := flag.Args()
+	for _, path := range paths {
+		err := processFile(path)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func processFile(path string) error {
+	fp, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	src, err := ioutil.ReadAll(fp)
+	if err != nil {
+		return err
+	}
+	return format(path, src)
+}
+
+func format(filename string, src []byte) error {
+	var fset *token.FileSet = token.NewFileSet()
+	file, err := parser.ParseFile(fset, filename, src, parser.ParseComments)
+	if err != nil {
+		return err
+	}
+	a, err := imports.FixImports(fset, file, filename)
+	if err != nil {
+		return err
+	}
+	fmt.Println(a, err)
+
+	cfg.Fprint(os.Stdout, fset, file)
+	return nil
+}
